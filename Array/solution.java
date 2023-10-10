3327baeb7e17cd0c73f1de64ecd34d2db812c6e6
pappu kumar <9262882257.pk@gmail.com>
2023-10-11 02:00:37 +0530

    add folder

---
+++ Array/solution.java
@@ -0,0 +1,31 @@
+package Array;
+
+// Java program to illustrate creating an array
+// of integers, puts some values in the array,
+// and prints each value to standard output.
+
+class solution {
+	public static void main(String[] args) {
+		// declares an Array of integers.
+		int[] arr;
+
+		// allocating memory for 5 integers.
+		arr = new int[5];
+
+		// initialize the first elements of the array
+		arr[0] = 10;
+
+		// initialize the second elements of the array
+		arr[1] = 20;
+
+		// so on...
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+
+		// accessing the elements of the specified array
+		for (int i = 0; i < arr.length; i++)
+			System.out.println("Element at index " + i
+					+ " : " + arr[i]);
+	}
+}
